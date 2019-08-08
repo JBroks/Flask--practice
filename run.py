a21a6a57f0932ef455405a43fc5f8feac8f214e1
Ubuntu <ubuntu@ip-172-31-85-182.ec2.internal>
2019-08-08 16:06:05 +0000

    Flask and data

--- run.py
+++ run.py
@@ -1,35 +1,28 @@
 import os
+import json
+from flask import Flask, render_template
 
-#We are importing the Flask class
-from flask import Flask
-
-"""
-We are creating instance of this and storing it in a variable called app
-The first argument of the Flask class is the name of the applications module - our package
-Since we are using a single module , we can use __name__ which is a built-in Python variable
-"""
 app = Flask(__name__)
 
-"""
-We're then using the app.route decorator. In Python, a decorator starts with the @ sign, which is
-also called pie notation.
-"""
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
-"""
-We can import "os" from the standard Python library, and
-then we're going to reference this built-in variable and say that
-if __name__ == "__main__":
-then we're going to run our app with the following
-arguments: the host is going to be set to os.environ.get("IP"). This is an
-internal environment variable that Cloud9 has set, and we're using the os
-module from the standard library to get that environment variable for us.
-It's the same with PORT, but this time we're casting it as an int, because it
-needs to be an integer. We're also then putting debug=true
-because that will allow us to debug our code easier. 
-"""
+@app.route("/about")
+def about():
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact") 
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers") 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
